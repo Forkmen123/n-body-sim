2d98e1a162e5f350f828a5a5001cfb2b3365168a
Tommy Roy <toroy20@ulaval.ca>
2026-01-01 22:29:09 -0500

    gitignore added

--- include/Body.hpp
+++ include/Body.hpp
@@ -5,21 +5,24 @@
 
 
 struct Body {
-    Vec3D position;
-    Vec3D vitesse;
-    Vec3D acceleration;
-
+    std::string _name;
+    double _mass;
+    Vec3D _pos;
+    Vec3D _vel;
+    std::string _color;
+    double _radius;
+    Vec3D _acc;
     std::vector<Vec3D> path;
-
-    private:
-        std::string _name;
-        std::string _color;
 
     Body(std::string name, double mass, Vec3D position, Vec3D velocity, std::string color, double radius = 0, Vec3D acceleration = Vec3D(0, 0, 0));
 
-    void update(double dt);
+    void update(double dt) {
+
+    }
        
-    void reset_acceleration();
+    void reset_acceleration() {
+
+    }
 };
 
 
--- src/Body.cpp
+++ src/Body.cpp
@@ -1,12 +1,22 @@
 #include "Vec3D.hpp"
+#include "Body.hpp"
 #include <cmath>
 #include <iostream>
 #include <string>
 
 
-Body::Body(std::string name, double mass, Vec3D position, Vec3D velocity, std::string color, double radius = 0, Vec3D acceleration ) {
-    _name = name;
-    _color = color;
+Body::Body(std::string name, double mass, Vec3D position, Vec3D velocity, std::string color, double radius, Vec3D acceleration) : 
+    _name(name), 
+    _mass(mass), 
+    _pos(position), 
+    _vel(velocity), 
+    _color(color), 
+    _radius(radius), 
+    _acc(acceleration)
+{
+
+
+
 }
 
 
